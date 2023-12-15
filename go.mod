23cb7fb042f502bd7a1eb3b02a7903a59e4afee9
Enrico Zada <enricozada@icloud.com>
2023-12-16 01:47:07 +0700

    upgrade major

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/enrico-zd/go-say-hello-tutor
+module github.com/enrico-zd/go-say-hello-tutor/v2
 
 go 1.21.2
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello_tutor
 
-func SayHello(name string) string {
-	return "Hello " + name
+func SayHello(name string, age int) string {
+	return "Hello " + name + " Umur " + age
 }
